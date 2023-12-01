9397b6264847ccd8c50270c8d5aea275c46e0dee
t <timofey.brayko@gmail.com>
2023-12-01 15:34:41 +0000

    Worked version(20-60 min)

---
+++ Generator.py
@@ -0,0 +1,110 @@
+import os
+import random
+import shutil
+import string
+import matplotlib.pyplot as plt
+import numpy as np
+
+from main import solve
+
+
+def generate_random_word(length):
+	letters = string.ascii_lowercase
+	random_word = ''.join(random.choice(letters) for _ in range(length))
+	return random_word
+
+
+def generator():
+	with open("words.txt", 'r') as file:
+		words = [word.strip() for word in file.readlines()]
+
+	for file_num in range(25, 100):
+		path = f"test_inputs/input{file_num}.txt"
+		file_data = []
+		created = False
+		while not created:
+			words_num = random.randint(5, 20)
+			with open(path, 'w') as file:
+				for word in range(words_num):
+					flag = True
+
+					while flag:
+						rand_word = random.choice(words)
+						if rand_word not in file_data:
+							flag = False
+							file_data.append(rand_word)
+							file.write(f"{rand_word}\n")
+
+			if solve(path, file_num):
+				shutil.move(path, f"inputs/input{file_num}.txt")
+				created = True
+
+
+def check_similarity():
+	def check_duplicates(file_path, i):
+		flag = False
+		word_set = set()
+		with open(file_path, 'r') as file:
+			for line_number, line in enumerate(file, start=1):
+				words = line.strip().split()
+
+				for word in words:
+					if word in word_set:
+						flag = True
+						print(f"Duplicate word '{word}' found in file {i}")
+					else:
+						word_set.add(word)
+
+	for i in range(0, 25):
+		name = f"inputs/input{i}.txt"
+		check_duplicates(name, i)
+
+
+def cases():
+	for i in range(0, 25):
+		arr = []
+		name = f"input{i}.txt"
+		if name in os.listdir("inputs"):
+			with open(f"inputs/{name}", "r") as file:
+				for word in file.readline():
+					arr.append(word.strip().lower())
+
+			with open(f"inputs/{name}", "w") as file:
+				for word in arr:
+					file.write(word)
+
+
+def find():
+	for i in os.listdir("inputs"):
+		with open(f"inputs/{i}", "r") as file:
+			words = [word.strip() for word in file.readlines()]
+			if len(words) > 17:
+				for word in words:
+					print(word)
+			print("Next")
+
+
+def avg_words():
+	sums = 0
+	for inp in os.listdir("inputs"):
+		with open(f"inputs/{inp}", "r") as file:
+			sums += len([word.strip() for word in file.readlines()])
+	print(sums / len(os.listdir("inputs")))
+
+
+def build():
+	np.random.seed(seed=0)
+	w_5 = [45, 52, 54, 13, 253, 12, 43]
+	w_10 = [90, 92, 192, 82, 12, 49, 23, 4, 3, 3]
+	a = (w_5, w_10)
+
+	fig, ax = plt.subplots()
+	ax.boxplot(a, vert=True, showmeans=False, meanline=False,
+			   labels=('5', '10'))
+	plt.show()
+
+
+# TODO change cases in inputs
+# TODO Check that input doesn't contains duplicates
+if __name__ == '__main__':
+	build()
--- main.py
+++ main.py
@@ -15,10 +15,10 @@ class Orientation(Enum):
 class META:
 	grid_size = 20
 	mutation_per = 0.3
-	crossover_per = 0.6
+	crossover_per = 0.6 / 2
 	old_best_per = 0.1
 
-	generations = 1000
+	generations = 80
 	population_size = 500
 
 	basic_score = 0
@@ -35,9 +35,8 @@ class Word:
 		self.position: tuple = None
 		self.end: tuple = None
 		self.visited: bool = False
-		self.valid = True
 
-	def __lt__(self, other):
+	def __lt__(self, other) -> bool:
 		return self.length > other.length
 
 	def overlap(self, other) -> bool:
@@ -49,124 +48,169 @@ class Word:
 				return True
 		return False
 
+	def are_intersect(self, other) -> bool | tuple[int]:
+		x1, y1 = self.position
+		x2, y2 = self.end
+		x3, y3 = other.position
+		x4, y4 = other.end
+		# For Lines with One Horizontal and One Vertical
+		if (x1 == x2 and y3 == y4) or (y1 == y2 and x3 == x4):
+			if x1 == x2:
+				if min(x3, x4) <= x1 <= max(x3, x4) and min(y1, y2) <= y3 <= max(y1, y2):
+					return x1, y3
+			elif y1 == y2:
+				if min(y3, y4) <= y1 <= max(y3, y4) and min(x1, x2) <= x3 <= max(x1, x2):
+					return x3, y1
+		return False
 
-def find_intersection_point(word1, word2):
-	x1, y1 = word1.position
-	x2, y2 = word1.end
-	x3, y3 = word2.position
-	x4, y4 = word2.end
-	# For Lines with One Horizontal and One Vertical
-	if (x1 == x2 and y3 == y4) or (y1 == y2 and x3 == x4):
-		if x1 == x2:
-			if min(x3, x4) <= x1 <= max(x3, x4) and min(y1, y2) <= y3 <= max(y1, y2):
-				return x1, y3
-		elif y1 == y2:
-			if min(y3, y4) <= y1 <= max(y3, y4) and min(x1, x2) <= x3 <= max(x1, x2):
-				return x3, y1
-	return False
+	def random_position(self):
+		self.orientation = random.choice([Orientation.Horizontal, Orientation.Vertical])
+		if self.orientation == Orientation.Horizontal:
+			x = random.randint(0, META.grid_size - self.length)
+			y = random.randint(0, META.grid_size - 1)
+			x_ = x + self.length - 1
+			y_ = y
+		else:
+			x = random.randint(0, META.grid_size - 1)
+			y = random.randint(0, META.grid_size - self.length)
+			x_ = x
+			y_ = y + self.length - 1
+		self.position = (x, y)
+		self.end = (x_, y_)
 
 
-def dfs(word: Word):
-	word.visited = True
-	for word_ in word.intersections.values():
-		if not word_.visited:
-			dfs(word_)
+class Crossword:
 
+	def __init__(self, words):
+		self.words: list[Word] = words
+		self.words_num = len(self.words)
 
-def fitness(individual):
-	crossword = copy.deepcopy(individual)
-	score = META.basic_score
-	for word in crossword:
-		for word_ in crossword:
-			if word_.string == word.string:
-				continue
+	def dfs(self, word: Word):
+		word.visited = True
+		for word_ in word.intersections.values():
+			if not word_.visited:
+				self.dfs(word_)
 
-			if word_.orientation == word.orientation:
+	def fitness(self) -> int:
+		crossword = copy.deepcopy(self.words)
+		score = META.basic_score
+		for word in crossword:
+			for word_ in crossword:
+				if word_.string == word.string:
+					continue
+
+				if word_.orientation == word.orientation:
+					par_1, par_2 = (1, 0) if word.orientation == Orientation.Vertical else (0, 1)
+
+					if word.overlap(word_):
+						score -= META.penalty
+					elif word_.position[par_2] in (word.position[par_2] + 1, word.position[par_2] - 1):
+						if (word.position[par_1] <= word_.position[par_1] < word.end[par_1]
+								or word.position[par_1] < word_.end[par_1] <= word.end[par_1]):
+							score -= META.penalty
+					elif word_.position[par_2] == word.position[par_2]:
+						if word_.end[par_1] == word.position[par_1] - 1 or word_.position[par_1] == word.end[par_1] + 1:
+							score -= META.penalty
+				else:
+					intersect = word.are_intersect(word_)
+					vert_w = word if word.orientation == Orientation.Vertical else word_
+					horiz_w = word_ if word.orientation == Orientation.Vertical else word
+
+					if intersect:
+						if (vert_w.string[intersect[1] - vert_w.position[1]]
+								!= horiz_w.string[intersect[0] - horiz_w.position[0]]):
+							score -= META.penalty
+						else:
+							word.intersections[intersect] = word_
+					else:
+						if vert_w.position[0] in (horiz_w.position[0] - 1, horiz_w.end[0] + 1):
+							if vert_w.position[1] <= horiz_w.position[1] <= vert_w.end[1]:
+								score -= META.penalty
+						elif horiz_w.position[1] in (vert_w.position[1] - 1, vert_w.end[1] + 1):
+							if horiz_w.position[0] <= vert_w.position[0] <= horiz_w.end[0]:
+								score -= META.penalty
+
+		# Check the case of parallel neighbour words, for first and last symbols in the words
+		for word in crossword:
+			for word_ in crossword:
+				if word_.string == word.string or word_.orientation != word.orientation:
+					continue
+
 				par_1, par_2 = (1, 0) if word.orientation == Orientation.Vertical else (0, 1)
 
-				if word.overlap(word_):
+				if word_.position[par_2] in (word.position[par_2] + 1, word.position[par_2] - 1):
+					if word_.position[par_1] == word.end[par_1]:
+						if not word.intersections.get(word.end) or not word_.intersections.get(word_.position):
+							score -= META.penalty
+					elif word.position[par_1] == word_.end[par_1]:
+						if not word.intersections.get(word.position) or not word_.intersections.get(word_.end):
+							score -= META.penalty
+
+		for i in range(self.words_num):
+			self.dfs(crossword[i])
+			for word in crossword:
+				if not word.visited or len(word.intersections) == 0:
 					score -= META.penalty
-
-				elif word_.position[par_2] in (word.position[par_2] + 1, word.position[par_2] - 1):
-					if (word.position[par_1] <= word_.position[par_1] < word.end[par_1]
-							or word.position[par_1] < word_.end[par_1] <= word.end[par_1]):
-						score -= META.penalty
-				elif word_.position[par_2] == word.position[par_2]:
-					if word_.end[par_1] == word.position[par_1] - 1 or word_.position[par_1] == word.end[par_1] + 1:
-						score -= META.penalty
-			else:
-				intersect = find_intersection_point(word, word_)
-				vert_w = word if word.orientation == Orientation.Vertical else word_
-				horiz_w = word_ if word.orientation == Orientation.Vertical else word
-
-				if intersect:
-					if (vert_w.string[intersect[1] - vert_w.position[1]]
-							!= horiz_w.string[intersect[0] - horiz_w.position[0]]):
-						score -= META.penalty
-					else:
-						word.intersections[intersect] = word_
-						word_.intersections[intersect] = word
 				else:
-					if vert_w.position[0] in (horiz_w.position[0] - 1, horiz_w.end[0] + 1):
-						if vert_w.position[1] <= horiz_w.position[1] <= vert_w.end[1]:
-							score -= META.penalty
-					elif horiz_w.position[1] in (vert_w.position[1] - 1, vert_w.end[1] + 1):
-						if horiz_w.position[0] <= vert_w.position[0] <= horiz_w.end[0]:
-							score -= META.penalty
+					word.visited = False
+		return score
 
-	for word in crossword:
-		for word_ in crossword:
-			if word_.string == word.string or word_.orientation != word.orientation:
-				continue
-
-			par_1, par_2 = (1, 0) if word.orientation == Orientation.Vertical else (0, 1)
-
-			if word_.position[par_2] in (word.position[par_2] + 1, word.position[par_2] - 1):
-				if word_.position[par_1] == word.end[par_1]:
-					if not word.intersections.get(word.end) or not word_.intersections.get(word_.position):
-						score -= META.penalty
-				elif word.position[par_1] == word_.end[par_1]:
-					if not word.intersections.get(word.position) or not word_.intersections.get(word_.end):
-						score -= META.penalty
-
-	# for i in range(len(crossword)):
-	dfs(crossword[0])
-	for word in crossword:
-		if not word.visited or len(word.intersections) == 0:
-			score -= META.penalty
-		else:
-			word.visited = False
-	return score
-
-
-def mutation(crossword):
-	individual = copy.deepcopy(crossword)
-	for i in range(3):
+	def mutation(self):
+		individual = copy.deepcopy(self.words)
 		rand_gen = random.choice(individual)
-		random_position(rand_gen)
-	return individual
+		rand_gen.random_position()
+		return Crossword(individual)
+
+	# TODO fix
+	def crossover(self, parent2) -> tuple[Word]:
+		def two_points():
+			# FIXME not working for 3 or less words.txt
+			point1, point2 = sorted(random.sample(range(1, len(self.words) - 1), 2))
+			offspring1 = Crossword(self.words[:point1] + parent2.words[point1:point2] + self.words[point2:])
+			offspring2 = Crossword(parent2.words[:point1] + self.words[point1:point2] + parent2.words[point2:])
+			return offspring1, offspring2
+
+		def mixed():
+			# Not working!
+			offspring1 = copy.deepcopy(self.words)
+			offspring2 = copy.deepcopy(parent2.words)
+			for index in range(self.words_num):
+				if random.choice([True, False]):
+					offspring1[index], offspring2[index] = offspring2[index], offspring1[index]
+			return offspring1, offspring2
+
+		def one_point():
+			# Not working!
+			point = random.randint(0, self.words_num - 1)
+			offspring1 = self.words[:point] + parent2[point:]
+			offspring2 = parent2[:point] + self.words[point:]
+			return offspring1, offspring2
+
+		return two_points()
+
+	def print(self):
+		grid = []
+		for _ in range(META.grid_size):
+			grid.append(['-' for _ in range(META.grid_size)])
+		for word in self.words:
+			x, y = word.position
+			for i in range(word.length):
+				if word.orientation == Orientation.Horizontal:
+					grid[y][x + i] = word.string[i]
+				else:
+					grid[y + i][x] = word.string[i]
+
+		print("+" + "-" * (META.grid_size * 3 - 1) + "+")
+		for row in grid:
+			print("|" + "  ".join(row) + "|")
+		print("+" + "-" * (META.grid_size * 3 - 1) + "+")
+
+	def insert_word(self, word: Word):
+		self.words.append(word)
+		self.words_num += 1
 
 
-def crossover(parent1, parent2):
-	def two_points():
-		# FIXME not working for 3 or less words.txt
-		point1, point2 = sorted(random.sample(range(1, len(parent1) - 1), 2))
-		offspring1 = parent1[:point1] + parent2[point1:point2] + parent1[point2:]
-		offspring2 = parent2[:point1] + parent1[point1:point2] + parent2[point2:]
-		return offspring1, offspring2
-
-	def mixed():
-		offspring1 = copy.deepcopy(parent1)
-		offspring2 = copy.deepcopy(parent2)
-		for index in range(len(parent1)):
-			if random.choice([True, False]):
-				offspring1[index], offspring2[index] = offspring2[index], offspring1[index]
-		return offspring1, offspring2
-
-	return mixed()
-
-
-def best_old_individuals(population, fitness_arr):
+def best_old_individuals(population: list[Crossword], fitness_arr: list[int]) -> list[Crossword]:
 	def get_max_indices(arr):
 		n = round(len(population) * 0.3)
 		sorted_indices = sorted(range(len(arr)), key=lambda k: arr[k])
@@ -179,199 +223,90 @@ def best_old_individuals(population, fitness_arr):
 	return best_ind
 
 
-def next_generation(population, fitness_arr):
+def evolve(population: list[Crossword], fitness_arr: list[int]) -> list[Crossword]:
 	best_old = best_old_individuals(population, fitness_arr)
 	n = round(len(population) * META.old_best_per)
 
 	new_population = copy.deepcopy(best_old[:n])
 
-	for _ in range(round((len(population) * META.crossover_per) / 2)):
-		offspring1, offspring2 = crossover(random.choice(best_old), random.choice(best_old))
+	for _ in range(round((len(population) * META.crossover_per))):
+		offspring1, offspring2 = random.choice(best_old).crossover(random.choice(best_old))
 		new_population.append(offspring1)
 		new_population.append(offspring2)
 
 	for _ in range(round(len(population) * META.mutation_per)):
-		new_population.append(mutation(random.choice(population)))
+		kek = random.choice(population)
+		new_population.append(kek.mutation())
+
+	for cr in new_population:
+		if not isinstance(cr, Crossword):
+			print(0)
 
 	return new_population
 
 
-def print_crossword(population):
-	grid = []
-	for _ in range(META.grid_size):
-		grid.append(['-' for _ in range(META.grid_size)])
-	for word in population:
-		x, y = word.position
-		for i in range(word.length):
-			if word.orientation == Orientation.Horizontal:
-				grid[y][x + i] = word.string[i]
-			else:
-				grid[y + i][x] = word.string[i]
-
-	print("+" + "-" * (META.grid_size * 3 - 1) + "+")
-	for row in grid:
-		print("|" + "  ".join(row) + "|")
-	print("+" + "-" * (META.grid_size * 3 - 1) + "+")
-
-
-def best(population, fitness_arr):
+def best_fit_crossword(population: list[Crossword], fitness_arr: list[int]) -> tuple[Crossword, int]:
 	fit_arr = copy.deepcopy(fitness_arr)
 	ind = fit_arr.index(max(fit_arr))
 	return population[ind], fit_arr[ind]
 
 
-def random_position(word):
-	word.orientation = random.choice([Orientation.Horizontal, Orientation.Vertical])
-	if word.orientation == Orientation.Horizontal:
-		x = random.randint(0, META.grid_size - word.length)
-		y = random.randint(0, META.grid_size - 1)
-		x_ = x + word.length - 1
-		y_ = y
-	else:
-		x = random.randint(0, META.grid_size - 1)
-		y = random.randint(0, META.grid_size - word.length)
-		x_ = x
-		y_ = y + word.length - 1
-	word.position = (x, y)
-	word.end = (x_, y_)
-
-
-def init_zero_population(w: list[Word]) -> list:
+def init_zero_population(input_words: Crossword) -> list[Crossword]:
 	zero_population = []
-	for i in range(META.population_size):
-		words = copy.deepcopy(w)
+	for _ in range(META.population_size):
+		words = copy.deepcopy(input_words)
 		for word in words:
-			random_position(word)
-		zero_population.append(words)
+			word.random_position()
+		zero_population.append(Crossword(words=words))
 	return zero_population
 
 
-def read_file(file_name):
-	with open(file_name, "r") as file:
-		return [Word(word.strip()) for word in file.readlines()]
-
-
-# def output(file_name, solved, crossword):
-# 	with open(f"outputs/{file_name}.txt", 'w') as out:
-# 		if solved:
-# 			for word in crossword:
-# 				out.write(f"{word.position[0]} {word.position[1]} {word.orientation.value[0]}")
-# 		else:
-# 			out.write("Not successful")
-
-
-# def solve(file_name):
-# 	words = read_file(file_name)
-# 	print(f"Words: {len(words)}", end="  ")
-# 	population = init_zero_population(words)
-# 	x_data = []
-# 	y_data = []
-#
-# 	loss_arr = []
-#
-# 	for generation in range(META.generations):
-# 		fitness_arr = [fitness(individual) for individual in population]
-# 		best_crossword, best_fitness = best(population, fitness_arr)
-# 		population = next_generation(population, fitness_arr)
-#
-# 		if best_fitness == 0:
-# 			plt.ylabel('Avr Loss')
-# 			plt.xlabel('Generations')
-# 			plt.scatter(x_data, y_data)
-# 			plt.show()
-# 			return True
-#
-# 		avr_loss = abs(sum(fitness_arr)) / len(fitness_arr)
-# 		loss_arr.append(avr_loss)
-#
-# 		x_data.append(generation)
-# 		y_data.append(avr_loss)
-#
-# 		if generation % 15 == 0:
-# 			plt.ylabel('Avr Loss')
-# 			plt.xlabel('Generations')
-# 			plt.scatter(x_data, y_data)
-# 			plt.show()
-#
-# 		if len(loss_arr) >= 40:
-# 			if loss_arr[-1] >= 10:
-# 				num = 40
-# 				threshold = 0.3
-# 			elif loss_arr[-1] >= 5:
-# 				num = 40
-# 				threshold = 0.3
-# 			else:
-# 				num = min(50, len(loss_arr))
-# 				threshold = 0.2
-# 			arr = [abs(loss_arr[i] - loss_arr[i - 1]) <= threshold for i in
-# 				   range(len(loss_arr) - num, len(loss_arr))]
-# 			if all(arr):
-# 				# print("Restart")
-# 				population = init_zero_population(words)
-#
-# 	plt.ylabel('Avr Loss')
-# 	plt.xlabel('Generations')
-# 	plt.scatter(x_data, y_data)
-# 	plt.show()
-# 	print("Bad", end="  ")
-
-
-def create_new_population(best_crossword, w):
+def create_new_population(best_crossword: Crossword, new_word: Word):
 	population = []
 	for _ in range(META.population_size):
 		crossword = copy.deepcopy(best_crossword)
-		word = copy.deepcopy(w)
-		random_position(word)
-		crossword.append(word)
+		word = copy.deepcopy(new_word)
+		word.random_position()
+		crossword.insert_word(word)
 		population.append(crossword)
 	return population
 
 
-def add_new_word(population, new_word):
-	for crossword in population:
-		word = copy.deepcopy(new_word)
-		crossword.append(word)
-		random_position(crossword[-1])
-	return population
+def read_file(file_name) -> Crossword:
+	with open(file_name, "r") as file:
+		return [Word(word.strip()) for word in file.readlines()]
 
-
-def test_solve(file_name):
+def solve(file_name):
 	words = sorted(read_file(file_name))
 	print("Words:", len(words), end=" ")
 
 	while True:
-		index = 2
-		four_words = copy.deepcopy(words[:2])
-		population = init_zero_population(four_words)
+		index = 4
+		initial_words_set = copy.deepcopy(words[:index])
+		population = init_zero_population(initial_words_set)
 
-		generation = 0
-		while generation < META.generations:
-			fitness_arr = [fitness(individual) for individual in population]
-			best_crossword, best_fitness = best(population, fitness_arr)
-
-			population = next_generation(population, fitness_arr)
+		for generation in range(META.generations):
+			fitness_arr = [individual.fitness() for individual in population]
+			best_crossword, best_fitness = best_fit_crossword(population, fitness_arr)
+			population = evolve(population, fitness_arr)
 			if best_fitness == 0:
-				if len(words) == len(population[0]):
+				if len(words) == population[0].words_num:
 					return len(words), abs(sum(fitness_arr)) / len(fitness_arr)
-				generation = 0
 				new_word = copy.deepcopy(words[index])
-				population = create_new_population(population, new_word)
-
-			if generation > 200:
-				print("Again", end=" ")
-				break
-			generation += 1
+				population = create_new_population(best_crossword, new_word)
+				index += 1
 
 
+# TODO Make class for Crossword
 if __name__ == "__main__":
 	words_arr = []
 	loss_arr = []
 	time_arr = []
 
-	tik = time.time()
-	for i in os.listdir("inputs"):
+	total_t = time.time()
+	for file in os.listdir("sh_inputs"):
 		timer = time.time()
-		words, loss = test_solve(f"inputs/{i}")
+		words, loss = solve(f"sh_inputs/{file}")
 		timer = time.time() - timer
 
 		loss_arr.append(loss)
@@ -382,7 +317,7 @@ if __name__ == "__main__":
 		if len(sec) == 1:
 			sec = "0" + sec
 		print(f"Time: {int(timer // 60)}:{sec}")
-	print("Total time: ", time.time() - tik)
+	print("Total time: ", time.time() - total_t)
 
 	plt.ylabel('Avr Loss')
 	plt.xlabel('Words')
